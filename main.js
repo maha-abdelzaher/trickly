df6ee54d25534fdcc349c1826dd3c0a12334b0cc
maha-abdelzaher <maha.abdelzaher@gmail.com>
2022-04-20 14:59:16 +0200

    fix error

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import mainscript from './assets/js/mainscript.js';
+// import './assets/js/mainscript.js';
 // import './assets/css/style.css'
 // import './assets/css/all.min.css'
 
-mainscript();
